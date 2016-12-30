fe50fb6fa9fb1962dfa1d1832c56275d345a275a
N <bigestyjoy@163.com>
2016-12-30 17:46:21 +0800

    Ver5.1.3.1
    完善域模型属性特性。

--- EmergencyInformationSystem/Models/Domains/Entities/RescueRoomInfo.cs
+++ EmergencyInformationSystem/Models/Domains/Entities/RescueRoomInfo.cs
@@ -450,6 +450,7 @@ namespace EmergencyInformationSystem.Models.Domains.Entities
         /// <summary>
         /// 床位名称-完整。
         /// </summary>
+        [Display(Name = "床位")]
         public string BedNameFull
         {
             get
