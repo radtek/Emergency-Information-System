10438cd10398e5e11ff5b95250ef3cf74347149c
N <bigestyjoy@163.com>
2017-07-11 09:56:47 +0800

    Ver5.3.15.0
    在报表系统实现床位预约表。

---
+++ EmergencyInformationReportSystem/Controllers/ReportController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmergencyInformationReportSystem.Controllers
+{
+    public class ReportController : Controller
+    {
+        public ActionResult IndexSubscription()
+        {
+            return View();
+        }
+    }
+}
