f6498c9019664c884f6f50b0f90644e896b57f87
N <bigestyjoy@163.com>
2017-09-05 17:14:18 +0800

    Ver5.3.21.1
    去除多余属性。

--- EmergencyInformationSystem/Models/ViewModels/RescueRoomInfos/Edit/Edit.cs
+++ EmergencyInformationSystem/Models/ViewModels/RescueRoomInfos/Edit/Edit.cs
@@ -13,14 +13,6 @@ namespace EmergencyInformationSystem.Models.ViewModels.RescueRoomInfos.Edit
         {
             this.RescueRoomInfoId = target.RescueRoomInfoId;
 
-            this.PatientName = target.PatientName;
-            this.OutPatientNumber = target.OutPatientNumber;
-            this.Sex = target.Sex;
-            this.ReceiveAgeName = target.ReceiveAgeName;
-            this.DiagnosisNameOrigin = target.DiagnosisNameOrigin;
-            this.ReceiveTime = target.ReceiveTime;
-            this.FirstDoctorName = target.FirstDoctorName;
-
             this.InDepartmentTime = target.InDepartmentTime;
             this.BedId = target.BedId;
             this.FirstNurseName = target.FirstNurseName;
@@ -63,32 +55,6 @@ namespace EmergencyInformationSystem.Models.ViewModels.RescueRoomInfos.Edit
 
 
         public int RescueRoomInfoId { get; set; }
-
-
-
-
-
-        [Display(Name = "患者姓名")]
-        public string PatientName { get; set; }
-
-        [Display(Name = "卡号")]
-        public string OutPatientNumber { get; set; }
-
-        [Display(Name = "性别")]
-        public string Sex { get; set; }
-
-        [Display(Name = "就诊年龄")]
-        public string ReceiveAgeName { get; set; }
-
-        [Display(Name = "入室诊断")]
-        public string DiagnosisNameOrigin { get; set; }
-
-        [Display(Name = "接诊时间")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")]
-        public DateTime? ReceiveTime { get; set; }
-
-        [Display(Name = "首诊医师")]
-        public string FirstDoctorName { get; set; }
 
 
 
